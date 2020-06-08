235ed3d98e90bd54014a585d4988daf385b8fa56
appac <appac@125.134.157.51>
2020-06-08 22:48:46 +0900

    1. 자바의 기초 이론

---
+++ android_java/src/org/_1_android_java/_1_Java_basic.java
@@ -0,0 +1,25 @@
+package org._1_android_java;
+
+public class _1_Java_basic {
+	
+	// J2SE (java 2 platform, standard edition)
+	// - 자바의 가장 기본이 되는 에디션
+	// - 모든 자바 프로그램은 J2SE에서 제공되는 컴파일러와 JRE를 사용한다.
+	
+	// J2ME (java 2 platform, micro edition)
+	// - 모바일 장비를 위한 에디션
+	// - 휴대폰에 가장 적합한 버전
+	// - 개발 시 사용할 J2SE의 버전은 1.3 이나 1.4 버전이 가장 적합함
+	// - WIPI : JAVA언어를 기본으로 만들어진 국내 이통3사 통합 플랫폼
+	//          J2ME와 유사한 부분이 상당히 많지만 다른점도 가지고 있다 
+	// - Android : 스마트폰 전용 운용체저. J2ME 제품 군에는 속하지 않으며, JAVA기반으로 만들어진 새로운 운영체제.
+	
+	// J2EE (java 2 platform, enterprise edition)
+	// - 기업용 환경에서 서비스하기 위한 어플리케이션을 개발할 떄 사용되는 버전
+	// - 기본 J2SE가 필요하며 최신 버전의 J2SE를 사용하는 것을 권장한다.
+	// - 주로 Web Application Service를 만들 때, 사용되며 그 밖에 다양한 기술들을 지원.
+	// - Servlet : Web Service 를 위해 제공되는 기술.
+	// - JSP(java server page) : 웹 서비스를 위해 제공되는 기술,
+	
+	
+}
